af08fb9f2070c858135ae1e6949c1a5fb9a29e81
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-06-01 16:09:33 +1000

    Add Docstrings to functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,7 @@
 """Get password insure it is valid and then display starts for each character"""
 
 def main():
+    """Gets valid password and masks it"""
     password_length = 10
     password = get_password(password_length)
 
@@ -8,10 +9,12 @@ def main():
 
 
 def display_masked_password(password, s='*'):
+    """Masks password"""
     print(s * len(password))
 
 
 def get_password(password_length):
+    """Gets the users password and check if valid"""
     password = input("Enter password: ")
     while len(password) < password_length:
         print(f"Password must be at least {password_length} characters long")
