507a75bd35a09a97bdd91cdd887a31dcb56d2dcd
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-06-08 19:23:46 +1000

    Answer prac questions and amend code to uses LBYL for ZeroDivisionError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -7,7 +7,6 @@ On lune 13 or 14 when converting user input into a integer if non-numbers where 
 If 0 was entered for the denominator
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 Yes you can yous LBYl and uses a while loop to prompt for new input until a non-zero integer is entered
-# there was a error with the commit this is a comment to test
 """
 
 try:
