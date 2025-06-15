188b2e3e1b5264dce0ea8263d072198305f67157
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-06-15 10:36:17 +1000

    Optimises solution for getting all numbers in list except the first one

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,7 +11,7 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # numbers + [6, 5, 3] - answer [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 numbers[0] = "ten"
 numbers[-1] = 1
-print(numbers[slice(1,-1)])
+print(numbers[2:])
 if 9 in numbers:
     print("9 is in numbers")
 else:
