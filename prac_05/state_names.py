b7de0a7ae4c3a8304381fb42c77b9233db2a3653
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-06-21 17:49:08 +1000

    Added formated output for displaying cases, handling of lower cases inputs and changed from LBYl to EAFP

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,15 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+for state_abbreviation, State_name in CODE_TO_NAME.items():
+    print(f"{state_abbreviation} is {State_name}")
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
