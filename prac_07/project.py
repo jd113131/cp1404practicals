1c09caf7738f18cab806c60345cff0761f7d4201
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-07-20 17:26:01 +1000

    Add loading of projects

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,5 +9,8 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        return f"{self.name} {self.start_date} {self.priority} {self.cost_estimate} {self.completion_percentage}"
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion_percentage}"
+
+    def __repr__(self):
+        return '('+self.__str__()+"\n)"
 
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,6 +3,9 @@ Programming project management task
 Estimate: 50:00 minutes
 Actual: 25:10 minutes
 """
+from project import Project
+
+FILENAME = "projects.txt"
 
 MAIN_MENU = """- (L)oad projects  
 - (S)ave projects  
@@ -14,14 +17,15 @@ MAIN_MENU = """- (L)oad projects
 
 
 def main():
-    """Creat a menu in which uses can manage projects."""
+    """Create a menu in which uses can manage projects."""
     print("Welcome to Pythonic Project Management")
-    ## TODO load projects
+    projects = load_projects(FILENAME)
     print(MAIN_MENU)
     choice = input(">>>").upper()
     while choice != "Q":
         if choice == "L":
-            pass
+            filename = input("Enter project file name:")
+            projects = load_projects(filename)
         elif choice == "S":
             pass
         elif choice == "D":
@@ -34,6 +38,22 @@ def main():
             pass
         elif choice == "Q":
             pass
+        # print(projects) # check if projects loaded
         choice = input(">>>").upper()
+
+def load_projects(filename):
+    """Load projects from a file."""
+    projects = []
+    with open(filename, "r") as infile:
+        infile.readline() # read first heading line
+        for line in infile:
+            parts = line.strip().split("\t")
+            parts[2] = int(parts[2])
+            parts[3] = float(parts[3])
+            parts[4] = float(parts[4])
+            projects.append(Project(*parts))
+    print(f"Loaded {len(projects)} projects from {filename}")
+    return projects
+
 
 main()
