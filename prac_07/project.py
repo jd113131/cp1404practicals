b573c08b08a2c4436f7867872876f3f2eafee685
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-07-20 17:09:29 +1000

    Add Project class

---
+++ prac_07/project.py
@@ -0,0 +1,13 @@
+"""Create a project management class"""
+
+class Project:
+    def __init__(self, name="", start_date="", priority=0, cost_estimate=0.0,completion_percentage=0.0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"{self.name} {self.start_date} {self.priority} {self.cost_estimate} {self.completion_percentage}"
+
