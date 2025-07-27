691a73727d13c80576aea9357f7c77cfcc3d1be2
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-07-27 23:53:24 +1000

    Perform final file cleanup

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
+
 class DynamicLabelsApp(App):
     """Main program - Kivy app to demo dynamic widget creation."""
 
@@ -24,5 +25,6 @@ class DynamicLabelsApp(App):
         for name in self.names:
             temp_label = Label(text=name)
             self.root.ids.main.add_widget(temp_label)
+
 
 DynamicLabelsApp().run()
