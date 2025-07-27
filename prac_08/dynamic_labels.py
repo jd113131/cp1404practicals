6569e2de96dc8a4c9ed31c36d77bcc29868ef9b1
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-07-27 23:35:04 +1000

    Add prac 8 task 4 dynamic labels exercise

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,28 @@
+"""Create a kivy GUI using dynamic labels"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+class DynamicLabelsApp(App):
+    """Main program - Kivy app to demo dynamic widget creation."""
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = ["Jon", "Jack", "Bob", "James", "Pat", "Linsay"]
+
+    def build(self):
+        """Build main app."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create labels."""
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+DynamicLabelsApp().run()
