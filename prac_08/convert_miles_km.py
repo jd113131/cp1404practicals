41b9b4f78bc13eb99913a1c88bfd547531feffe3
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-07-27 22:16:50 +1000

    Add prac 8 task 3 part 2 error validation and immediate response to distance

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+CONVERTION_FACTOR = 1.609
 
 class MilesToKmApp(App):
     distance_in_km = StringProperty()
@@ -13,14 +14,20 @@ class MilesToKmApp(App):
         """build kivy app from kv file"""
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
-        self.distance_in_km = "0"
+        self.distance_in_km = ""
         return self.root
 
     def update_distance(self, distance_in_miles, increment_value):
-        new_distance_miles = (int(distance_in_miles) + increment_value)
+        try:
+            new_distance_miles = (int(distance_in_miles) + increment_value)
+        except ValueError:
+            new_distance_miles = increment_value
         self.root.ids.distance_miles.text = str(new_distance_miles)
 
     def convert(self, distance_in_miles):
-        self.distance_in_km = str(float(distance_in_miles) * 1.609)
+        try:
+            self.distance_in_km = str(float(distance_in_miles) * CONVERTION_FACTOR)
+        except ValueError:
+            self.distance_in_km = "0.0"
 
 MilesToKmApp().run()
