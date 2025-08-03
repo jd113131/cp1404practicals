02a2ebc9f013045bdeae0aa2f8caeb7ef05547ef
jd113131 <jonathan.kenneally@my.jcu.edu.au>
2025-08-04 00:06:37 +1000

    Add taxi ride fare calculation and taxi selection

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,34 +1,65 @@
 """Taxi simulator."""
-from prac_01.temperatures import choice
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
 
 MAIN_MENU = "q)uit, c)hoose taxi, d)rive"
+
 
 def main():
     """Menu program that allows users to add taxi rides."""
     current_taxi = None
-    taxis = []
+    cost = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     print(MAIN_MENU)
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
-            # TODO add choose logic
-            pass
+            current_taxi = select_taxi(taxis, current_taxi)
         elif choice == "d":
             if current_taxi is None:
                 print("You need to choose a taxi before you can drive")
             else:
-                # TODO add logic
-                pass
+                cost += taxi_ride(taxis, current_taxi)
             pass
         else:
             print("Invalid option")
 
-
+        print(f"Bill to date: ${cost:.2f}")
         print(MAIN_MENU)
         choice = input(">>> ").lower()
 
-    # TODO final message
-    print()
+    print(f"Total trip cost: ${cost}")
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    """Display the taxis information."""
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+def taxi_ride(taxis, current_taxi):
+    distance = int(input("Drive how far? "))
+    taxis[current_taxi].drive(distance)
+    price = taxis[current_taxi].get_fare()
+    taxis[current_taxi].start_fare()  # reset for next time
+    return price
+
+
+def select_taxi(taxis, old_taxi):
+    """Select a taxi based on user choice."""
+    print("Taxis available:")
+    display_taxis(taxis)
+    try:
+        current_taxi = int(input("Choose taxi: "))
+        if 0 <= current_taxi < len(taxis):
+            return current_taxi
+    except ValueError:
+        pass
+    print("Invalid option")
+    return old_taxi
+
 
 main()
